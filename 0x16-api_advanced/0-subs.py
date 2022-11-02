9e358b15ff387b422b457c42537ff29d3c37eb8f
Adunni <otungprincess@gmail.com>
2022-11-01 17:06:32 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Function to query subscribers on a given Reddit subreddit. """
+
+import json
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ Returns the number of subscribers for a given subreddit """
+    user_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    subs = response.json()
+    if 'data' not in subs:
+        return 0
+    if 'subscribers' not in subs.get('data'):
+        return 0
+    return response.json()['data']['subscribers']
